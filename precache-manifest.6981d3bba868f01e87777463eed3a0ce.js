bf0452d0705fcc37307cb972b84e52172228fd0c
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-10 19:21:53 +0300

    Updates

---
+++ precache-manifest.6981d3bba868f01e87777463eed3a0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e163d379096fdac1a16e0942cd9c9a63",
+    "url": "/News-App-with-Redux-Saga/index.html"
+  },
+  {
+    "revision": "f57bc63433f1c2465134",
+    "url": "/News-App-with-Redux-Saga/static/css/main.d4027325.chunk.css"
+  },
+  {
+    "revision": "456305112cce1dcc1f53",
+    "url": "/News-App-with-Redux-Saga/static/js/2.6d5ad3d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News-App-with-Redux-Saga/static/js/2.6d5ad3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57bc63433f1c2465134",
+    "url": "/News-App-with-Redux-Saga/static/js/main.c3762c06.chunk.js"
+  },
+  {
+    "revision": "e64e2c1463228b0059d0",
+    "url": "/News-App-with-Redux-Saga/static/js/runtime-main.d9bfcf2c.js"
+  }
+]);
